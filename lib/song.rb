cc9a2ed06345012654b18ed814b2bff8b02e3b5c
Abi Raskin <abiraskin@gmail.com>
2018-11-15 03:13:28 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  def @@album_count = 0
+
+  def initialize
+    @@album_count += 1
+  end
+    
+  def self.count
+    @@album_count
+  end
+end    
