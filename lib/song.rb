4da9970d651d3850bf9fdc0a729a4805f88ce5df
Abi Raskin <abiraskin@gmail.com>
2018-11-15 03:25:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end
 
   def self.count
@@ -13,7 +20,7 @@ class Song
   end
 
 one_day = Song.new
-
+  
 end
 
 one_day = Song.new
